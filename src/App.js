9541855d1cb33bdb2d4203547058411678a18cf4
lera-ro <lera.rodoman@yandex.ru>
2024-08-25 21:57:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ const [place, setPlace] = useState(data);
+ const [text, setText] = useState(false);
 
+ const removeCity = (id) => {
+  let newThing = place.filter(thing => thing.id !== id);
+  setPlace(newThing);
+ }
+
+ const showText = (item) =>{
+  item.showMore = !item.showMore;
+  setText(!text);
+ }
+
+ return(<div>
+  <div className='container'>
+    <h1>{place.length} interesting cities in Russia</h1>
+  </div>
+
+  {place.map(item => {
+    const {id, city, description, source, image, showMore} = item;
+    return(<div>
+      <div className='container'>
+        <h2>{id} - {city}</h2>
+      </div>
+
+      <div className='container'>
+        <img src={image} width="400px" alt="city"/>
+      </div>
+
+      <div className='container'>
+        <p>{showMore ? description : description.substring(0, 50) + "..."} 
+          <button onClick={() => showText(item)} className='submit'>{showMore ? 'меньше' : 'больше'}</button></p>
+      </div>
+
+      <div className='container'>
+        <p>{source}</p>
+      </div>
+
+      <div className='container'>
+        <button onClick={() => removeCity(id)} className='btn'>удалить</button>
+      </div>
+    </div>)
+  }
+)}
+ <div className='container'>
+  <button onClick={() => setPlace([])} className='btn all'>удалить все</button>
+ </div>
+ </div>)
+  
+}
 export default App;
